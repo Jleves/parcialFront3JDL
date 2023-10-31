78963e43a711850a029e65111e823ac7e60cd7d1
jleves <jleves@iupa.edu.ar>
2023-10-31 20:56:43 -0300

    Template examen Jorge Leves

---
+++ src/Components/Formulario.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Formulario = () => {
+  return (
+    <div>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+    </div>
+  )
+}
+
+export default Formulario
